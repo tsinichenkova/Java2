fe503fff6642a74741ffab5f37033ee26951fe9f
Tatyana Sinichenkova <t.sinichenkova@infinnity.ru>
2021-11-24 21:12:03 +0500

    добавлено 2 дз

---
+++ src/main/java/ru/geekbrains/qa/java2/lesson2/MyArrayDataException.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.qa.java2.lesson2;
+
+public class MyArrayDataException extends Exception{
+    private int i;
+    private int j;
+
+    public MyArrayDataException(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+}
